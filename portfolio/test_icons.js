6a0708d029d4f844ff3c0ac01877351688bc213c
Afsal Rahiman T <afsalrahimant1@gmail.com>
2025-12-02 23:05:38 +0530

    Update portfolio 2025-12-02 23:05

--- portfolio/test_icons.js
+++
@@ -1,5 +0,0 @@
-import { SiJupyter } from 'react-icons/si';
-import { FaNetworkWired } from 'react-icons/fa';
-
-console.log('SiJupyter:', SiJupyter);
-console.log('FaNetworkWired:', FaNetworkWired);
